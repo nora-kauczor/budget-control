2188672990d0b35b59bc8625f25ae568e2c31456
Nora Kauczor <n.kauczor@web.de>
2024-12-09 22:12:49 +0100

    use OAuth user as in argument in month plan controller

--- backend/src/main/java/org/example/backend/MonthPlanController.java
+++ backend/src/main/java/org/example/backend/MonthPlanController.java
@@ -1,6 +1,8 @@
 package org.example.backend;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,27 +15,27 @@ public class MonthPlanController {
     private final MonthPlanService monthPlanService;
 
     @GetMapping("/{id}")
-    public MonthPlan getMonthPlanOfUser(@PathVariable String id) {
-        return monthPlanService.getMonthPlanOfUser(id);
+    public MonthPlan getMonthPlanOfUser(@AuthenticationPrincipal OAuth2User user, @PathVariable String id) {
+        return monthPlanService.getMonthPlanOfUser(user, id);
     }
 
     @GetMapping
-    public List<MonthPlan> getAllMonthPlansOfUser() {
-        return monthPlanService.getAllMonthPlansOfUser();
+    public List<MonthPlan> getAllMonthPlansOfUser(@AuthenticationPrincipal OAuth2User user) {
+        return monthPlanService.getAllMonthPlansOfUser(user);
     }
 
     @PostMapping
-    public MonthPlan createMonthPlan(@RequestBody MonthPlanDTO monthPlanDTO) {
-        return monthPlanService.createMonthPlan(monthPlanDTO);
+    public MonthPlan createMonthPlan(@AuthenticationPrincipal OAuth2User user, @RequestBody MonthPlanDTO monthPlanDTO) {
+        return monthPlanService.createMonthPlan(user, monthPlanDTO);
     }
 
     @PutMapping
-    public MonthPlan editMonthPlan(@RequestBody MonthPlan editedMonthPlan){
-        return monthPlanService.editMonthPlan(editedMonthPlan);
+    public MonthPlan editMonthPlan(@AuthenticationPrincipal OAuth2User user, @RequestBody MonthPlan editedMonthPlan){
+        return monthPlanService.editMonthPlan(user, editedMonthPlan);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteMonthPlan(@PathVariable String id){
-        return monthPlanService.deleteMonthPlan(id);
+    public String deleteMonthPlan(@AuthenticationPrincipal OAuth2User user, @PathVariable String id){
+        return monthPlanService.deleteMonthPlan(user, id);
     }
 }
